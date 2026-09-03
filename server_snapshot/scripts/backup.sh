8102cf591a15cb2d74ecf857ce9b09bc0befd3fb
Yaroslav <yaroslav@Yaroslavs-MacBook-Pro.local>
2026-09-04 01:16:05 +0300

    backup.sh: бэкапы деплоев пишутся вне веб-корня (crm_backups_predeploy), backups/ из webroot убран при чистке сервера

--- server_snapshot/scripts/backup.sh
+++ server_snapshot/scripts/backup.sh
@@ -3,7 +3,7 @@
 set -e
 
 APP_DIR="/var/www/h212005/data/www/87-232-64-12.nip.io"
-BACKUP_DIR="$APP_DIR/backups"
+BACKUP_DIR="/var/www/h212005/data/crm_backups_predeploy"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 BACKUP_NAME="backup_$TIMESTAMP"
 
